b6b1f732b1977b69c876a09b5d18242ce2732565
farhan-jabil <farhanhasan295@gmail.com>
2024-11-03 12:46:36 +0600

    working on about

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,32 @@ const About = () => {
   return (
     <>
       <div className="container py-5">
-        <div className="section-header">
-          <div>ABOUT US</div>
-          <div>Serving Since 1950</div>
+        <div className="section-header relative text-center pt-28 pb-9">
+          <div className="text-2xl tracking-[5px] text-[#DA9F5B]">ABOUT US</div>
+          <div className="text-5xl font-bold">Serving Since 1950</div>
+        </div>
+        <div>
+          <div>
+            <div className="mb-4 text-4xl">Our Story</div>
+            <div className="text-xl">
+              Eos kasd eos dolor vero vero, lorem stet diam rebum. Ipsum amet
+              sed vero dolor sea
+            </div>
+            <div className="">
+              Takimata sed vero vero no sit sed, justo clita duo no duo amet et,
+              nonumy kasd sed dolor eos diam lorem eirmod. Amet sit amet amet
+              no. Est nonumy sed labore eirmod sit magna Erat at est justo sit
+              ut. Labor diam sed ipsum et eirmod
+            </div>
+            <div>
+              <button
+                type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Default
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
