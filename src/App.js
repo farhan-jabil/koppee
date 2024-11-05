e2efab95b16fce7bc1a4f076e1cf5882baa58d06
farhanOkobiz <farhan.okobiz@gmail.com>
2024-11-05 21:37:31 +0600

    adding offer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import Offer from "./components/Offer";
 import Reservation from "./components/Reservation";
 import Services from "./components/Services";
 import Testimonial from "./components/Testimonial";
@@ -19,6 +20,7 @@ function App() {
         <div className="py-10"/>
         <Services></Services>
         <div className="py-10"/>
+        <Offer></Offer>
         {/* <Menu></Menu> */}
         {/* <Reservation></Reservation> */}
         {/* <Testimonial></Testimonial> */}
