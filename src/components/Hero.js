fe66d1aba30007f404df2975059b60a78017caba
farhan-jabil <farhanhasan295@gmail.com>
2024-11-21 00:56:00 +0600

    hero done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,55 +4,82 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { images } from "../utils/demo_images";
 
-// const PrevArrow = ({ onClick }) => {
-//   return (
-//     <button
-//       className="absolute text-2xl left-20 top-1/2 transform -translate-y-1/2 z-10 text-white rounded-full p-2"
-//       onClick={onClick}
-//     >
-//       <i class="fa-solid fa-chevron-left"></i>
-//     </button>
-//   );
-// };
+const PrevArrow = ({ onClick }) => (
+  <button
+    className="absolute left-5 md:left-10 top-1/2 transform -translate-y-1/2 z-20 text-xl md:text-3xl font-bold text-white p-3 rounded-full"
+    onClick={onClick}
+    aria-label="Previous Slide"
+  >
+    <i className="fas fa-chevron-left"></i>
+  </button>
+);
 
-// const NextArrow = ({ onClick }) => {
-//   return (
-//     <button
-//       className="absolute text-2xl right-20 top-1/2 transform -translate-y-1/2 z-10 text-white rounded-full p-2"
-//       onClick={onClick}
-//     >
-//       <i class="fa-solid fa-chevron-right"></i>
-//     </button>
-//   );
-// };
+const NextArrow = ({ onClick }) => (
+  <button
+    className="absolute right-5 md:right-10 top-1/2 transform -translate-y-1/2 z-20 text-xl md:text-3xl font-bold text-white p-3 rounded-full"
+    onClick={onClick}
+    aria-label="Next Slide"
+  >
+    <i className="fas fa-chevron-right"></i>
+  </button>
+);
 
 const Hero = () => {
-  
-  const sliderImages = [images.hero1, images.hero2];
+  const slides = [
+    {
+      image: images.hero1,
+      text: {
+        subtitle: "We Have Been Serving",
+        title: "Coffee",
+        footer: "*Since 1950*",
+      },
+    },
+    {
+      image: images.hero2,
+      text: {
+        subtitle: "Experience The Best",
+        title: "Brew",
+        footer: "*Crafted With Love*",
+      },
+    },
+  ];
 
   const settings = {
     infinite: true,
     speed: 200,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
-    // prevArrow: <PrevArrow />,
-    // nextArrow: <NextArrow />,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
   };
 
   return (
     <div className="slider-container w-full overlay-bottom relative">
       <Slider {...settings}>
-        <div className="slider-container-elements relative w-full">
-          <div>
-            <img src={images.hero1} className="w-full" alt="Hero 1" />
+        {slides.map((slide, index) => (
+          <div key={index} className="slider-container-elements relative w-full">
+            <div>
+              <img
+                src={slide.image}
+                className="w-full h-[60vh] md:h-[75vh] lg:h-screen object-cover"
+                alt={`Slide ${index + 1}`}
+              />
+            </div>
+            <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center space-y-3 px-4">
+              <div className="text-[#DA9F5B] font-medium text-lg md:text-2xl">
+                {slide.text.subtitle}
+              </div>
+              <div className="uppercase text-white text-5xl md:text-7xl lg:text-9xl font-bold leading-tight">
+                {slide.text.title}
+              </div>
+              <div className="uppercase text-lg md:text-xl lg:text-2xl text-white">
+                {slide.text.footer}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="slider-container-elements relative w-full">
-          <div>
-            <img src={images.hero2} className="w-full" alt="Hero 2" />
-          </div>
-        </div>
+        ))}
       </Slider>
     </div>
   );
